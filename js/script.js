6ce362055734612bf30acc548fcce334e8724661
vl <euggrush@gmail.com>
2020-06-05 20:59:34 -0700

    add

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 let modal = document.querySelector('.modal');
 let hotelSearchBtn = document.querySelector('.hotel-search');
+
+hotelSearchBtn.addEventListener('click', function (event) {
+    let target = event.target;
+    if (target && target.classList.contains('hotel-search')) {
+       modal.classList.remove('modal-hidden');
+    }
+});
