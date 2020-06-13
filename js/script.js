9e86a1a4fe9693d8a2027be4000c7e6f02bfa63a
Eugene Grushin <38574843+euggrush@users.noreply.github.com>
2020-06-13 00:41:01 -0700

    add

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let modal = document.querySelector('.modal');
 let hotelSearchBtn = document.querySelector('.hotel-search');
 
-hotelSearchBtn.addEventListener('click', function (event) {
+hotelSearchBtn.addEventListener('click', function(event) {
     let target = event.target;
     if (target && target.classList.contains('hotel-search')) {
-       modal.classList.remove('modal-hidden');
+        modal.classList.toggle('modal-hidden');
     }
 });
