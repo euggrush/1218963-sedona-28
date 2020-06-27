b169e304d35d2ace8f1a1276f65066b12a34ed56
Eugene Grushin <38574843+euggrush@users.noreply.github.com>
2020-06-27 14:49:12 -0700

    add

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let modal = document.querySelector('.modal');
 let hotelSearchBtn = document.querySelector('.hotel-search');
 let dateInInput = document.querySelector('.in-input');
+let dateOutInput = document.querySelector('.out-input');
+let adultsNumber = document.querySelector('.adults-number');
+let childrenNumber = document.querySelector('.children-nubmer');
 
 hotelSearchBtn.addEventListener('click', function(event) {
     let target = event.target;
