ed6ea9c93eb9cbbc4ae5b990a768125a43d67e01
Eugene Grushin <38574843+euggrush@users.noreply.github.com>
2020-06-28 10:12:27 -0700

    add

--- js/catalog-filter.js
+++ js/catalog-filter.js
@@ -2,13 +2,15 @@ let checkboxBtn = document.querySelectorAll(".filter-checkbox-wrapper");
 let checkboxIcon = document.querySelectorAll(".checkbox-on-off");
 
 
-
 for (let i = 0; i < checkboxBtn.length; i++) {
     checkboxBtn[i].addEventListener('click', function (event) {
       let target = event.target;
       if (target.classList.contains('checkbox-filter')) {
-        checkboxIcon.classList.remove('swim-checkbox-icon-off');
-        checkboxIcon.classList.add('swim-checkbox-icon-on');
+        // checkboxIcon[i].classList.remove('swim-checkbox-icon-off');
+        // checkboxIcon[i].classList.add('swim-checkbox-icon-on');
+
+        checkboxIcon[i].classList.toggle('swim-checkbox-icon-on');
+        // console.log('Hello');
       }
     });
   }
